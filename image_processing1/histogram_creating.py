f8f16ab173e383191b125f3b96d75469f3b7cfe4
Anton Pashkouski <Wolendrang@gmail.com>
2016-09-13 20:46:54 +0300

    Histogram creating init.

---
+++ image_processing1/histogram_creating.py
@@ -0,0 +1,17 @@
+def show_histograms(original_image, first_image, second_image, third_image):
+    if original_image.mode == 'L':
+        pass
+    elif original_image.mode == 'RGB':
+        pass
+
+
+def __show_histograms_3byte_images(original_image, first_image, second_image, third_image):
+    pass
+
+
+def __show_histograms_1byte_images(original_image, first_image, second_image, third_image):
+    pass
+
+
+if __name__ == '__main__':
+    pass
